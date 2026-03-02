77d8d59bef66ecc3028788e64e0f3781d9c4415b
Karisa07 <karen.isaza@correounivalle.edu.co>
2026-03-01 19:48:36 -0500

    Modificación final a App

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CalculatorApp());
+  runApp(const CalculadoraApp());
 }
 
-class CalculatorApp extends StatelessWidget {
-  const CalculatorApp({super.key});
+class CalculadoraApp extends StatelessWidget {
+  const CalculadoraApp({super.key});
 
   @override
   Widget build(BuildContext context) {
